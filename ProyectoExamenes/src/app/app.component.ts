99801a07a41d2d4983d6a0f863b7537b3d57a4e1
yeida2916 <amadeo565@gmail.com>
2024-10-19 11:49:54 +0200

    Incluido componente en app

--- ProyectoExamenes/src/app/app.component.ts
+++ ProyectoExamenes/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UploadFileComponent } from "./components/upload-file/upload-file.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UploadFileComponent],
   templateUrl: './app.component.html',
+  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
