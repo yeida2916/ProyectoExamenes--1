f70b028b3ee83d07a41c415197adda4e7966da8b
yeida2916 <amadeo565@gmail.com>
2024-11-06 11:46:08 +0100

    componente pantalla mostrar descarga pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MyComponent } from './components/my-component';
 import { ExamListComponent } from './pages/exam-list/exam-list.component';
+import { DescargaPdfComponent } from './pages/pantalla-descarga-pdf/descarga-pdf.component';
 import { TestGeneratorComponent } from './pages/test-generator/test-generator.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, ExamListComponent, MyComponent, TestGeneratorComponent,],
+  imports: [RouterOutlet, RouterModule, ExamListComponent, MyComponent, TestGeneratorComponent,DescargaPdfComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
